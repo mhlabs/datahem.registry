60c25b2369a23f591397b5e9f80c6c8cc9ce7075
ml-engineer <robert.sahlin@mathem.se>
2021-10-26 13:58:38 +0200

    cloudbuild

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # datahem.registry
 A wrapper API for AWS glue schema registry.
+
+## Deploy
+Save AWS credentials as GCP Secrets (gcp-aws-schema-secret-key, gcp-aws-schema-key-id) that you can reference from cloud functions as environment variables. Make sure the service account has access to the secrets, i.e. <project>@appspot.gserviceaccount.com has Secret Manager Secret Accessor.
+
+Make sure cloud build service account has roles "Cloud Functions Admin" and "Service Account User".
+
+```sh
+gcloud beta functions deploy subjectVersionFn \
+    --region europe-west1 \
+    --trigger-http \
+    --allow-unauthenticated \
+    --runtime java11 \
+    --entry-point org.datahem.registry.SubjectVersion \
+    --memory 512MB \
+    --set-env-vars AWS_REGION=eu-west-1 \
+    --set-secrets=AWS_SECRET_ACCESS_KEY=projects/${PROJECT_ID}/secrets/gcp-aws-schema-secret-key:latest,AWS_ACCESS_KEY_ID=projects/${PROJECT_ID}/secrets/gcp-aws-schema-key-id:latest
+
+```
--- src/main/java/org/datahem/registry/SubjectVersion.java
+++ src/main/java/org/datahem/registry/SubjectVersion.java
@@ -1,11 +1,12 @@
 package org.datahem.registry;
 
-// [START functions_helloworld_http]
+// [START functions_subjectVersion_http]
 
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
-import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Logger;
@@ -13,34 +14,30 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 import software.amazon.awssdk.services.glue.GlueClient;
-import software.amazon.awssdk.services.glue.model.GetSchemaRequest;
-import software.amazon.awssdk.services.glue.model.GetSchemaResponse;
 import software.amazon.awssdk.services.glue.model.GetSchemaVersionRequest;
 import software.amazon.awssdk.services.glue.model.GetSchemaVersionResponse;
 import software.amazon.awssdk.services.glue.model.SchemaId;
 import software.amazon.awssdk.services.glue.model.SchemaVersionNumber;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 
-public class HelloHttp implements HttpFunction {
-  private static final Logger logger = Logger.getLogger(HelloHttp.class.getName());
-
-  private static final Gson gson = new Gson();
+public class SubjectVersion implements HttpFunction {
+  private static final Logger logger = Logger.getLogger(SubjectVersion.class.getName());
 
   @Override
-  public void service(HttpRequest request, HttpResponse response)
-      throws IOException {
-    // Check URL parameters for "name" field
-    // "world" is the default value
-    String name = request.getFirstQueryParameter("name").orElse("world");
-
-    // Parse JSON request and check for "name" field
+  public void service(HttpRequest request, HttpResponse response) throws IOException {
+    JsonObject responsePayload =  new JsonObject();
     try {
       Pattern pattern = Pattern.compile("/subjects/(.*)/versions/(.*)");
       Matcher matcher = pattern.matcher(request.getPath());
+      
       if(matcher.matches()){
-        SchemaVersionNumber svn = SchemaVersionNumber.builder().latestVersion(true).build();
         String subject = matcher.group(1);
         String version = matcher.group(2);
+        SchemaVersionNumber svn = SchemaVersionNumber.builder()
+          .latestVersion(true)
+          .build();
+        
+        // check if version is latest or a version number
         if(!version.equals("latest")){
           try {
             long l = Long.parseLong(version);
@@ -53,13 +50,7 @@ public class HelloHttp implements HttpFunction {
         GlueClient gc = GlueClient.builder()
             .httpClient(ApacheHttpClient.builder().build())
             .build();
-        GetSchemaResponse gsr = gc.getSchema(GetSchemaRequest.builder()
-            .schemaId(SchemaId.builder()
-                .schemaName(subject)
-                .registryName("PocSchemaRegistry")
-                .build()
-            ).build());
-        logger.info(gsr.toString());
+
         GetSchemaVersionResponse gsvr = gc.getSchemaVersion(GetSchemaVersionRequest.builder()
           .schemaId(SchemaId.builder()
             .schemaName(subject)
@@ -68,15 +59,20 @@ public class HelloHttp implements HttpFunction {
           )
           .schemaVersionNumber(svn)
           .build());
-        logger.info(gsvr.toString());
-        name = gsvr.schemaDefinition();
+
+        responsePayload.addProperty("subject", subject);
+        responsePayload.addProperty("id", gsvr.schemaVersionId());
+        responsePayload.addProperty("version", gsvr.versionNumber());
+        responsePayload.addProperty("schemaType", gsvr.dataFormatAsString());
+        responsePayload.addProperty("schema", gsvr.schemaDefinition());
       }
     } catch (Exception e) {
       logger.severe("Error: " + e.getMessage());
     }
 
+    response.setContentType("application/json; charset=utf-8");
     var writer = new PrintWriter(response.getWriter());
-    writer.printf("SchemaDefiniton: %s", name);
+    writer.printf(responsePayload.toString());
   }
 }
-// [END functions_helloworld_http]
+// [END functions_subjectVersion_http]
